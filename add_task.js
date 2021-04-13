75fa59744969b7865fb3734b7aa29009383ae3f7
magdalena-o <magdalena.obermayr@gmx.at>
2021-04-13 16:51:48 +0200

    function createTask

---
+++ add_task.js
@@ -0,0 +1,23 @@
+let allTasks = [];
+
+function createTask() {
+    let title = document.getElementById('title');
+    let category = document.getElementById('category');
+    let description = document.getElementById('description');
+    let deadline = document.getElementById('submisson-date');
+    let urgency = document.getElementById('urgency');  
+
+    let task = {
+        'title': title.value,
+        'category': category.value,
+        'description': description.value,
+        'deadline': deadline.value,
+        'urgency': urgency.value 
+    };
+
+   allTasks.push(task);
+
+   title.value = '';
+   description.value = '';
+  
+}
