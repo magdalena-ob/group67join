d2c65cce1bb32bc0bed9ad878d9e3059d61eb304
HPBenjamin <hptischoff@gmail.com>
2021-04-15 22:06:08 +0200

    LogIn with some problems

---
+++ login.js
@@ -0,0 +1,36 @@
+let LogIn = [];
+let searchLogIn = [];
+function createUser() {
+    let newUser = document.getElementById('Username').value;
+    let newPassword = document.getElementById('Password').value;
+    LogIn.push({
+        'UserNames': newUser,
+        'Passwords': newPassword
+    });
+    justEntry();
+    clearInput();
+}
+
+function clearInput() {
+    document.getElementById('Username').value = ``;
+    document.getElementById('Password').value = ``;
+}
+
+function justEntry() {
+   window.location = "board.html";
+}
+
+function searchUser() {
+    let oldUsername = document.getElementById('Username').value;
+    let oldPassword = document.getElementById('Password').value;
+    searchLogIn.push({
+        'searchName': oldUsername,
+        'searchPassword': oldPassword
+    });
+    if (LogIn['UserNames'].indexOf(searchLogIn['oldUsername']) === -1 && LogIn['Passwords'].indexOf(searchLogIn['oldPassword']) === -1) {
+        console.log("irgendwas stimmt nicht!!!");
+    } else {
+        console.log("Gratuliere!!!");
+    }
+}
+
